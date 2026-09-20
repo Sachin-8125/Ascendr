cc5242602d9345bf52fbd91fa9dea355deb140ff
Sachin-8125 <devinewolf7@gmail.com>
2026-09-20 10:03:48 +0530

    feat: set up Express server and add rank routes

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/db.js";
 import authRouter from "./routes/authRoutes.js";
+import rankRouter from "./routes/rankRoutes.js";
 
 connectDB();
 
@@ -16,6 +17,7 @@ app.get("/", (req, res) => {
 });
 
 app.use('/api/auth', authRouter)
+app.use('/api/rank', rankRouter)
 
 const PORT = process.env.PORT || 5000;
 
